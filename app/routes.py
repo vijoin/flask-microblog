c69b589562397efd5977bfccea1816fab4c2e3d7
Victor Inojosa <vijoin@gmail.com>
2019-08-31 23:00:28 -0300

    [IMP] Show several posts from several users
    User a for loop to iterate through several posts in the template view

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    context = {'title': 'Home',
-               'username': 'Victor'}  # Mock user
+    context = {
+        'title': 'Home',
+        'username': 'Victor',  # Mock user
+        'posts': [
+            {
+                'author': {'username': 'Jhon'},
+                'body': 'Beautiful day in Portland!',
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!',
+            },
+        ]
+               }
     return render_template('index.html', **context)
