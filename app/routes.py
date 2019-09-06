f6a1df1ca32318c0e76b24dd4c392ef74b7af0a0
Victor Inojosa <vijoin@gmail.com>
2019-09-05 22:05:37 -0300

    [ADD] Requiring users to login
    Here we use the decorator @login_required to make sure
    that the user is authenticated or as him/her to login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
 from app.models import User
@@ -7,6 +8,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     context = {
         'title': 'Home',
@@ -36,7 +38,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
