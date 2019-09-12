8818ac49f12509af9beaa69d2f75471517820528
Victor Inojosa <vijoin@gmail.com>
2019-09-11 22:53:30 -0300

    Pass posts section to a subtemplate Create a page to edit profile

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic') #Here Post refers to Classname
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, username, email):
         self.username = username
